20cfd13cc58fa6e16a693dd09d447b2e0cf2da42
Digwit <hsieh357@gmail.com>
2016-08-03 13:11:49 -0500

    Update Parser.java

--- Parser.java
+++ Parser.java
@@ -1,136 +1,141 @@
-public class parser
+
+public class Parser
 {
-  public static void parseProgram(Tokenizer t)
-  {
-    parseStatements(t);
-  }
-  public static void parseStatements(Tokenizer t)
-  {
-    if (t.moreTokens())
-    {
-      parseStatement(t);
-      if (t.peekToken().getType()== T_SEMICOLON)
-        t.getToken();
-      else
-        System.out.println("Expected a semicolon! :(");
-      parseStatements(t);
-    }
-  }
-  
-  public static void parseStatement(Tokenizer t)
-  {
-    if (t.peekToken().getType() == T_LOAD)
-      parseLoadStatement(t);
-    else if(t.peekToken().getType() == T_DELETE)
-      parseDeleteStatement(t);
-    else if(t.peekToken().getType() == T_KEEP)
-      parseKeepStatement(t);
-    else if(t.peekToken().getType() == T_PRINT)
-      parsePrintStatement(t);
-    else if (t.peekToken().getType() == T_SAVE)
-      parsePrintStatement(t);
-    else if (t.peekToken().getType() == T_MERGE)
-      parsePrintStatement(t);
-  }
-  
-  public static void parseLoadStatement(Tokenizer t)
-  {
-    Token filename;
-    Token variable;
-    t.getToken();
-    if (t.peekToken().getType() == T_STRING)
-      filename = t.getToken();
-    else
-      System.out.println("You needed a filename.");
-    if (t.peekToken().getType() == T_INTO)
-      t.getToken();
-    else
-      System.out.println("You need the 'INTO' keyword...");
-    if (t.peekToken().getType() == T_VARIABLE)
-      variable = t.getToken();
-    else
-      System.out.println("You need a variable name for your table!");
-    //Do something with variable and filename that loads csv file into table
-  }
-  
-  public static void parseDeleteStatement(Tokenizer t)
-  {
-    Token expression;
-    Token variable;
-    int columns; //1 means column, 0 means row
-    t.getToken(); //Deletes 'DELETE' from strong
-    if (t.peekToken().getType() == T_RECORD)
-    {
-      t.getToken();
-      columns = 1; 
-    }
-    if (t.peekToken().getType() == T_FIELD)
-    {
-      t.getToken();
-      columns = 0;
-    }
-    else
-      System.out.println("You need to specify if you want to delete record(s)(rows) or field(s)(columns)");
-    
-    if (t.peekToken().getType() == T_STRING)
-      //Expression could be one of 3 options: integer; integer + "," + integer etc.; integer + "-" + integer
-      expression = t.getToken();
-    else
-      System.out.println("You need to specify what you want to delete.");
-    if (t.peekToken().getType() == T_FROM)
-      t.getToken();
-    else
-      System.out.println("You need the keyword 'FROM' to specify which table.");
-    if (t.peekToken().getType() == T_VARIABLE)
-      variable = t.getToken();
-    else
-      System.out.println("You need the name of the table you want to delete.");
-    //Somehow check expression contents and store if correct, then delete accordingly
-  }
-  
-  public static void parseKeepStatement(Tokenizer t)
-  {
-    Token expression;
-    Token variable;
-    int columns; //1 means column, 0 means row
-    t.getToken(); //Deletes 'DELETE' from strong
-    if (t.peekToken().getType() == T_RECORD)
-    {
-      t.getToken();
-      columns = 1; 
-    }
-    if (t.peekToken().getType() == T_FIELD)
-    {
-      t.getToken();
-      columns = 0;
-    }
-    else
-      System.out.println("You need to specify if you want to keep record(s)(rows) or field(s)(columns)");
-    
-    if (t.peekToken().getType() == T_STRING)
-      //Expression could be one of 3 options: integer; integer + "," + integer etc.; integer + "-" + integer
-      expression = t.getToken();
-    else
-      System.out.println("You need to specify what you want to keep.");
-    if (t.peekToken().getType() == T_FROM)
-      t.getToken();
-    else
-      System.out.println("You need the keyword 'FROM' to specify which table.");
-    if (t.peekToken().getType() == T_VARIABLE)
-      variable = t.getToken();
-    else
-      System.out.println("You need the name of the table you want to manipulate.");
-    //Somehow check expression contents and store if correct, then delete other records/fields accordingly
-  }
-  
-  public static void parsePrintStatement(Tokenizer t)
-  {
-    Token expression;
-    Token variable;
-    t.getToken();
-    //UHHH how do i do this
-   
-        
-        
-        
-        }
+	private Tokenizer myTokenizer;
+	public Parser(Tokenizer t)
+	{
+		myTokenizer = t;
+	}
+	public void parseProgram()
+	{
+		parseStatements();
+	}
+	public void parseStatements()
+	{
+		if (myTokenizer.moreTokens())
+		{
+			parseStatement();
+			if (myTokenizer.peekToken().getType() == Token.T_SEMICOLON)
+				myTokenizer.getToken();
+			else
+				System.out.println("Expected a semicolon! :(");
+			parseStatements();
+	}
+	}
+	
+	public void parseStatement()
+	{
+		if (myTokenizer.peekToken().getType() == Token.T_LOAD)
+			parseLoadStatement();
+		else if(myTokenizer.peekToken().getType() == Token.T_DELETE)
+			parseDeleteStatement();
+		else if(myTokenizer.peekToken().getType() == Token.T_KEEP)
+			parseKeepStatement();
+		/*
+		else if(myTokenizer.peekToken().getType() == Token.T_PRINT)
+			parsePrintStatement();
+			*/
+	}
+	
+	public void parseLoadStatement()
+	{
+		Token filename;
+		Token variable;
+		System.out.println(myTokenizer.peekToken());
+		myTokenizer.getToken();
+		System.out.println(myTokenizer.peekToken());
+		if (myTokenizer.peekToken().getType() == Token.T_STRING)
+			filename = myTokenizer.getToken();
+		else
+			System.out.println("You needed a filename.");
+		
+		System.out.println(myTokenizer.peekToken());
+		if (myTokenizer.peekToken().getType() == Token.T_INTO)
+			myTokenizer.getToken();
+		else
+			System.out.println("You need the 'INTO' keyword...");
+		
+		System.out.println(myTokenizer.peekToken());
+		if (myTokenizer.peekToken().getType() == Token.T_VARIABLE)
+			variable = myTokenizer.getToken();
+		else
+			System.out.println("You need a variable name for your table!");
+		//Do something with variable and filename that loads csv file into table
+	}
+	
+	public void parseDeleteStatement()
+	{
+		Token expression;
+		Token variable;
+		int columns; //1 means column, 0 means row
+		myTokenizer.getToken(); //Deletes 'DELETE' from strong
+		if (myTokenizer.peekToken().getType() == Token.T_RECORD)
+		{
+			myTokenizer.getToken();
+			columns = 1;	
+		}
+		if (myTokenizer.peekToken().getType() == Token.T_FIELD)
+		{
+			myTokenizer.getToken();
+			columns = 0;
+		}
+		else
+			System.out.println("You need to specify if you want to delete record(s)(rows) or field(s)(columns)");
+		
+		if (myTokenizer.peekToken().getType() == Token.T_STRING)
+			//Expression could be one of 3 options: integer; integer + "," + integer etc.; integer + "-" + integer
+			expression = myTokenizer.getToken();
+		else
+			System.out.println("You need to specify what you want to delete.");
+		if (myTokenizer.peekToken().getType() == Token.T_FROM)
+			myTokenizer.getToken();
+		else
+			System.out.println("You need the keyword 'FROM' to specify which table.");
+		if (myTokenizer.peekToken().getType() == Token.T_VARIABLE)
+			variable = myTokenizer.getToken();
+		else
+			System.out.println("You need the name of the table you want to delete.");
+		//Somehow check expression contents and store if correct, then delete accordingly
+	}
+	
+	public  void parseKeepStatement()
+	{
+		Token expression;
+		Token variable;
+		int columns; //1 means column, 0 means row
+		myTokenizer.getToken(); //Deletes 'DELETE' from strong
+		if (myTokenizer.peekToken().getType() == Token.T_RECORD)
+		{
+			myTokenizer.getToken();
+			columns = 1;	
+		}
+		if (myTokenizer.peekToken().getType() == Token.T_FIELD)
+		{
+			myTokenizer.getToken();
+			columns = 0;
+		}
+		else
+			System.out.println("You need to specify if you want to keep record(s)(rows) or field(s)(columns)");
+		
+		if (myTokenizer.peekToken().getType() == Token.T_STRING)
+			//Expression could be one of 3 options: integer; integer + "," + integer etc.; integer + "-" + integer
+			expression = myTokenizer.getToken();
+		else
+			System.out.println("You need to specify what you want to keep.");
+		if (myTokenizer.peekToken().getType() == Token.T_FROM)
+			myTokenizer.getToken();
+		else
+			System.out.println("You need the keyword 'FROM' to specify which table.");
+		if (myTokenizer.peekToken().getType() == Token.T_VARIABLE)
+			variable = myTokenizer.getToken();
+		else
+			System.out.println("You need the name of the table you want to manipulate.");
+		//Somehow check expression contents and store if correct, then delete other records/fields accordingly
+	}
+	
+	
+	
+	
+	
+	
+	}
