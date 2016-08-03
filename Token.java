6a07584c831b4bb9491e3a7c154f56c04515698d
Digwit <hsieh357@gmail.com>
2016-08-03 13:12:48 -0500

    Update Token.java

--- Token.java
+++ Token.java
@@ -1,5 +1,30 @@
 public class Token 
 {
+	public static final int T_LOAD = 1;
+	public static final int T_INTO = 2;
+	public static final int T_DELETE = 3;
+	public static final int T_KEEP = 4;
+	public static final int T_FROM = 5;
+	public static final int T_RECORD = 6;
+	public static final int T_FIELD = 7;
+	public static final int T_WHERE = 8;
+	public static final int T_STRING = 9;
+	public static final int T_NUMBER = 10;
+	public static final int T_VARIABLE= 11;
+	public static final int T_SEMICOLON = 12;
+	/*
+	public static final int T_("-") = 13;
+	public static final int T_(",") = 14;
+	public static final int T_("=") = 15;
+	public static final int T_(">") = 16;
+	public static final int T_("<") = 17;
+	public static final int T_(">=") = 18;
+	public static final int T_("<=") = 19;
+	public static final int T_("+") = 20;
+	public static final int T_("*") = 21;
+	public static final int T_("/") = 22;
+	*/
+	
  private String myValue;
  private int myType;
  
@@ -19,7 +44,11 @@ public class Token
   return myType;
  }
  
+ public String toString()
+ {
+	 return myValue + "[" + myType + "]";
  }
  
-}
+ }
+
 
