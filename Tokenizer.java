ca5acc69eb4a714e5b6a305fd9ab586b1017269f
Digwit <hsieh357@gmail.com>
2016-08-02 21:17:47 -0500

    Update Tokenizer.java

--- Tokenizer.java
+++ Tokenizer.java
@@ -8,68 +8,53 @@
    myProgram = program;
   }
   
-  public Token getToken()
-  {
-  public String getTokenString()
-  {
+public String getTokenString()
+ {
   String temp = " ";
   if(myValue.substring(0) == " ")
   {
-  String delStr = " ";
-  String newStr;
-  newStr =  myValue.replace(delStr, "");
-  return newStr;
+   String delStr = " ";
+   String newStr;
+   newStr =  myValue.replace(delStr, "");
+   return newStr;
   }
- 
+  
   else if(myValue.substring(0) == "\n")
   {
-  String delStr = "\n";
-  String newStr;
-  newStr =  myValue.replace(delStr, "");
-  return newStr;
+   String delStr = "\n";
+   String newStr;
+   newStr =  myValue.replace(delStr, "");
+   return newStr;
   }
   
   else
   {
-  String subStr1 = " ";
-  String subStr2 = "\n";
- 
-  int indexStr1 = myValue.indexOf(subStr1);
-  int indexStr2 = myValue.indexOf(subStr2);
-  
-  if(subStr1.equals(" "))
-  {
-  String getStr = myValue.substring(0, indexStr1);
-  
-  String delete1 = " ";
-  String new1;
-  new1 = myValue.replace(delete1, "");
-  temp = new1;
-  }
-  else if(subStr2.equals("\n"))
-  {
- String getStr = myValue.substring(0, indexStr2);
- 
- String delete2 = "\n";
- String new2;
-  new2 = myValue.replace(delete2, "");
-  temp = new2;
-  }
-  
-  return temp;
-  }
-  
-  }
-  
-  public boolean moreToken()
-  {
+   String subStr1 = " ";
+   String subStr2 = "\n";
    
-  }
-  
-  // similar to the front of queue
-  public Token peakToken()
-  {
+   int indexStr1 = myValue.indexOf(subStr1);
+   int indexStr2 = myValue.indexOf(subStr2);
    
+   if(subStr1.equals(" "))
+   {
+    String getStr = myValue.substring(0, indexStr1);
+    
+    String delete1 = " ";
+    String new1;
+    new1 = myValue.replace(delete1, "");
+    temp = new1;
+   }
+   else if(subStr2.equals("\n"))
+   {
+    String getStr = myValue.substring(0, indexStr2);
+
+    String delete2 = "\n";
+    String new2;
+    new2 = myValue.replace(delete2, "");
+    temp = new2;
+   }
+   
+   return temp;
   }
- 
  }
+ 
