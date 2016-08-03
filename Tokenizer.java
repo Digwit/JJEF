ac10729aca5968ebceb28b915c400e468be1205c
Digwit <hsieh357@gmail.com>
2016-08-02 20:38:20 -0500

    Create Tokenizer.java

---
+++ Tokenizer.java
@@ -0,0 +1,28 @@
+public class Tokenizer 
+{
+ private String myProgram;
+ 
+ // constructor 
+ public Tokenizer(String program)
+ {
+  myProgram = program;
+ }
+ 
+ public Token getToken()
+ {
+  String convert;
+  Token.getToken();
+ }
+ 
+ public boolean moreToken()
+ {
+  
+ }
+ 
+ // similar to the front of queue
+ public Token peakToken()
+ {
+  
+ }
+
+}
