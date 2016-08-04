f055b97bf0d0a399543722dfcf5d2f4fef695f2d
Digwit Eisenhower <hsieh357@gmail.com>
2016-08-03 20:38:40 -0500

    Update Parser.java

--- Parser.java
+++ Parser.java
@@ -3,26 +3,21 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-public class Parser
-{
+public class Parser {
 	private Tokenizer myTokenizer;
-	private Map <String, Table> tableVariables;
-	
-	
-	
-	public Parser(Tokenizer t)
-	{
+	private Map<String, Table> tableVariables;
+
+	public Parser(Tokenizer t) {
 		tableVariables = new HashMap<String, Table>();
 		myTokenizer = t;
 	}
-	public void parseProgram()
-	{
+
+	public void parseProgram() {
 		parseStatements();
 	}
-	public void parseStatements()
-	{
-		if (myTokenizer.moreTokens())
-		{
+
+	public void parseStatements() {
+		if (myTokenizer.moreTokens()) {
 			parseStatement();
 			if (myTokenizer.peekToken().getType() == Token.T_SEMICOLON)
 				myTokenizer.getToken();
@@ -32,29 +27,32 @@ public class Parser
 		}
 	}
 
-	public void parseStatement()
-	{
+	public void parseStatement() {
 		if (myTokenizer.peekToken().getType() == Token.T_LOAD)
 			parseLoadStatement();
-		else if(myTokenizer.peekToken().getType() == Token.T_DELETE)
+		else if (myTokenizer.peekToken().getType() == Token.T_DELETE)
 			parseDeleteStatement();
-		else if(myTokenizer.peekToken().getType() == Token.T_KEEP)
+		else if (myTokenizer.peekToken().getType() == Token.T_KEEP)
 			parseKeepStatement();
 		/*
-		else if(myTokenizer.peekToken().getType() == Token.T_PRINT)
-			parsePrintStatement();
+		 * else if(myTokenizer.peekToken().getType() == Token.T_PRINT)
+		 * parsePrintStatement();
 		 */
 	}
 
-	public void parseLoadStatement()
-	{
+	public void parseLoadStatement() {
 		Token filename = null;
 		Token variable = null;
 
 		myTokenizer.getToken();
-		
+
 		if (myTokenizer.peekToken().getType() == Token.T_STRING)
-			filename = myTokenizer.getToken();
+		{
+			String temp = myTokenizer.getToken().getValue();
+			temp = temp.substring(1);
+			temp = temp.substring(0, temp.length() -1);
+			filename = new Token(temp, Token.T_STRING);
+		}
 		else
 			parseError("You needed a filename.");
 
@@ -62,74 +60,64 @@ public class Parser
 			myTokenizer.getToken();
 		else
 			parseError("You need the 'INTO' keyword...");
-		
+
 		if (myTokenizer.peekToken().getType() == Token.T_VARIABLE)
 			variable = myTokenizer.getToken();
 		else
 			parseError("You need a variable name for your table!");
-		
-		Table table = new Table (Table.convert(filename.getValue()));
+
+		Table table = new Table(Table.convert(filename.getValue()));
 		tableVariables.put(variable.getValue(), table);
 	}
 
-	public void parseDeleteStatement()
+	public void parseDeleteStatement() 
 	{
 		Token expression = null;
 		Token variable = null;
 		List<Integer> expressions = null;
 		int range = 0;
-		
-		int columns = -1; //1 means column, 0 means row
-		myTokenizer.getToken(); //Deletes 'DELETE' from strong
-		if (myTokenizer.peekToken().getType() == Token.T_RECORD)
-		{
+
+		int columns = -1; // 1 means column, 0 means row
+		myTokenizer.getToken(); // Deletes 'DELETE' from strong
+		if (myTokenizer.peekToken().getType() == Token.T_RECORD) {
 			myTokenizer.getToken();
-			columns = 1;	
-		}
-		else if (myTokenizer.peekToken().getType() == Token.T_FIELD)
-		{
+			columns = 1;
+		} else if (myTokenizer.peekToken().getType() == Token.T_FIELD) {
 			myTokenizer.getToken();
 			columns = 0;
-		}
-		else if (myTokenizer.peekToken().getType() == Token.T_RECORDS)
-		{
+		} else if (myTokenizer.peekToken().getType() == Token.T_RECORDS) {
 			myTokenizer.getToken();
 			columns = 3;
-		}
-		else if (myTokenizer.peekToken().getType() == Token.T_FIELDS)
-		{
+		} else if (myTokenizer.peekToken().getType() == Token.T_FIELDS) {
 			myTokenizer.getToken();
 			columns = 2;
-		}
-		else
+		} else
 			parseError("You need to specify if you want to delete record(s)(rows) or field(s)(columns)");
 
 		if (myTokenizer.peekToken().getType() == Token.T_NUMBER)
-			//Expression could be one of 3 options: integer; integer + "," + integer etc.; integer + "-" + integer
+			// Expression could be one of 3 options: integer; integer + "," +
+			// integer etc.; integer + "-" + integer
 			expression = myTokenizer.getToken();
 		else
 			parseError("You need to specify what you want to delete.");
-		
-		if ((columns == 3) || (columns == 2))
-		{
-			if (myTokenizer.peekToken().getType() == Token.T_COMMA)
-			{
+
+		if ((columns == 3) || (columns == 2)) {
+			if (myTokenizer.peekToken().getType() == Token.T_COMMA) {
 				expressions = new ArrayList<Integer>();
 				expressions.add(Integer.parseInt(expression.getValue()));
-				
-				while (myTokenizer.peekToken().getType() == Token.T_COMMA)
-				{
+
+				while (myTokenizer.peekToken().getType() == Token.T_COMMA) {
 					myTokenizer.getToken();
 					if (myTokenizer.peekToken().getType() == Token.T_NUMBER)
-						//Expression could be one of 3 options: integer; integer + "," + integer etc.; integer + "-" + integer
+						// Expression could be one of 3 options: integer;
+						// integer + "," + integer etc.; integer + "-" + integer
 						expression = myTokenizer.getToken();
 					else
 						parseError("You need to specify what you want to delete.");
 					expressions.add(Integer.parseInt(expression.getValue()));
-				}	
+				}
 			}
-			if (myTokenizer.peekToken().getType() == Token.T_TO)
-			{
+			if (myTokenizer.peekToken().getType() == Token.T_TO) {
 				myTokenizer.getToken();
 				range = 1;
 				expressions.add(Integer.parseInt(expression.getValue()));
@@ -140,7 +128,7 @@ public class Parser
 				expressions.add(Integer.parseInt(expression.getValue()));
 			}
 		}
-		
+
 		if (myTokenizer.peekToken().getType() == Token.T_FROM)
 			myTokenizer.getToken();
 		else
@@ -149,56 +137,47 @@ public class Parser
 			variable = myTokenizer.getToken();
 		else
 			parseError("You need the name of the table you want to delete.");
-		
-		
-		if (columns == 0)
-		{
-			tableVariables.put(variable.getValue(), Table.deleteRow(tableVariables.get(variable.getValue()), Integer.parseInt(expression.getValue())));
-		}
-		else if (columns == 1)
-		{
-			tableVariables.put(variable.getValue(), Table.deleteColumn(tableVariables.get(variable.getValue()), Integer.parseInt(expression.getValue())));
-		}
-		else if (columns == 2)
-		{
-			if (range == 0)
-			{
-			int deleted = 0;
-			for (Integer e: expressions)
-			{
-				tableVariables.put(variable.getValue(), Table.deleteRow(tableVariables.get(variable.getValue()), e - deleted));
-			}
-			}
-			else if (range == 1)
-			{//NOT DONE
-				for (int k = 
-						expressions[1]; )
-				tableVariables.putAll(variable.getValue(), Table);
+
+		if (columns == 0) {
+			tableVariables.put(variable.getValue(),
+					Table.deleteRow(tableVariables.get(variable.getValue()), Integer.parseInt(expression.getValue())));
+		} else if (columns == 1) {
+			tableVariables.put(variable.getValue(), Table.deleteColumn(tableVariables.get(variable.getValue()),
+					Integer.parseInt(expression.getValue())));
+		} else if (columns == 2) {
+			if (range == 0) {
+				int deleted = 0;
+				for (Integer e : expressions) {
+					tableVariables.put(variable.getValue(),
+							Table.deleteRow(tableVariables.get(variable.getValue()), e - deleted));
+				}
+			} else if (range == 1) {
+				for (int k = expressions.get(1); k > expressions.get(0); k--) {
+					tableVariables.put(variable.getValue(),
+							Table.deleteRow(tableVariables.get(variable.getValue()), k));
+				}
 			}
 		}
 	}
 
-	public  void parseKeepStatement()
-	{
+	public void parseKeepStatement() {
 		Token expression = null;
 		Token variable = null;
-		int columns = -1; //1 means column, 0 means row
-		myTokenizer.getToken(); //Deletes 'DELETE' from strong
-		if (myTokenizer.peekToken().getType() == Token.T_RECORD)
-		{
+		int columns = -1; // 1 means column, 0 means row
+		myTokenizer.getToken(); // Deletes 'DELETE' from strong
+		if (myTokenizer.peekToken().getType() == Token.T_RECORD) {
 			myTokenizer.getToken();
-			columns = 1;	
+			columns = 1;
 		}
-		if (myTokenizer.peekToken().getType() == Token.T_FIELD)
-		{
+		if (myTokenizer.peekToken().getType() == Token.T_FIELD) {
 			myTokenizer.getToken();
 			columns = 0;
-		}
-		else
+		} else
 			parseError("You need to specify if you want to keep record(s)(rows) or field(s)(columns)");
 
 		if (myTokenizer.peekToken().getType() == Token.T_STRING)
-			//Expression could be one of 3 options: integer; integer + "," + integer etc.; integer + "-" + integer
+			// Expression could be one of 3 options: integer; integer + "," +
+			// integer etc.; integer + "-" + integer
 			expression = myTokenizer.getToken();
 		else
 			parseError("You need to specify what you want to keep.");
@@ -212,13 +191,9 @@ public class Parser
 			parseError("You need the name of the table you want to manipulate.");
 	}
 
-
-
-public void parseError(String message)
-{
-	System.out.println(message);
-	System.exit(1);
-}
-
+	public void parseError(String message) {
+		System.out.println(message);
+		System.exit(1);
+	}
 
 }
