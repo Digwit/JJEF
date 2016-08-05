b7db75fd70ee7b71e758e4a0e8ecf29a2d51bb19
Digwit Eisenhower <hsieh357@gmail.com>
2016-08-05 06:33:53 -0500

    Update Token.java

--- Token.java
+++ Token.java
@@ -1,5 +1,4 @@
-public class Token 
-{
+public class Token {
 	public static final int T_LOAD = 1;
 	public static final int T_INTO = 2;
 	public static final int T_DELETE = 3;
@@ -10,49 +9,43 @@ public class Token
 	public static final int T_WHERE = 8;
 	public static final int T_STRING = 9;
 	public static final int T_NUMBER = 10;
-	public static final int T_VARIABLE= 11;
+	public static final int T_VARIABLE = 11;
 	public static final int T_SEMICOLON = 12;
 	public static final int T_RECORDS = 13;
 	public static final int T_FIELDS = 14;
 	public static final int T_COMMA = 15;
 	public static final int T_TO = 16;
+	public static final int T_SAVE = 17;
+	public static final int T_PRINT = 18;
 	/*
-	public static final int T_("-") = 13;
-	public static final int T_(",") = 14;
-	public static final int T_("=") = 15;
-	public static final int T_(">") = 16;
-	public static final int T_("<") = 17;
-	public static final int T_(">=") = 18;
-	public static final int T_("<=") = 19;
-	public static final int T_("+") = 20;
-	public static final int T_("*") = 21;
-	public static final int T_("/") = 22;
-	*/
-	
- private String myValue;
- private int myType;
- 
- public Token(String value, int type)
- {
-  myValue = value;
-  myType = type;
- }
- 
- public String getValue()
- {
-  return myValue;
- }
- 
- public int getType() //Not sure how this works because in Parser I use this with T_SEMICOLON, not just integers
- {
-  return myType;
- }
- 
- public String toString()
- {
-	 return myValue + "[" + myType + "]";
- }
- 
- }
+	 * public static final int T_("-") = 13; public static final int T_(",") =
+	 * 14; public static final int T_("=") = 15; public static final int T_(">")
+	 * = 16; public static final int T_("<") = 17; public static final int
+	 * T_(">=") = 18; public static final int T_("<=") = 19; public static final
+	 * int T_("+") = 20; public static final int T_("*") = 21; public static
+	 * final int T_("/") = 22;
+	 */
 
+	private String myValue;
+	private int myType;
 
+	public Token(String value, int type) {
+		myValue = value;
+		myType = type;
+	}
+
+	public String getValue() {
+		return myValue;
+	}
+
+	public int getType() // Not sure how this works because in Parser I use this
+							// with T_SEMICOLON, not just integers
+	{
+		return myType;
+	}
+
+	public String toString() {
+		return myValue + "[" + myType + "]";
+	}
+
+}
